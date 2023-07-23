888195b93b14af7796b499534ff47e78f1ee0471
ColesonWelles <cahh38@protonmail.com>
2023-07-23 15:10:06 -0700

    extending the lexer & token set (2023-07-23)

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,10 +15,11 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
+
 !-/*5;
+
 5 < 10 > 5;
 `
-
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -59,6 +60,18 @@ let result = add(five, ten);
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
@@ -68,7 +81,7 @@ let result = add(five, ten);
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%qa",
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
 		if tok.Literal != tt.expectedLiteral {
