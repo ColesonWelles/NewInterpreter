af690fdd89c26fcdb1e385e86481d135f3d92120
ColesonWelles <cahh38@protonmail.com>
2023-08-11 14:52:17 -0700

    implementing bindings and env

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/ColesonWelles/NewInterpreter/evaluator"
 	"github.com/ColesonWelles/NewInterpreter/lexer"
+	"github.com/ColesonWelles/NewInterpreter/object"
 	"github.com/ColesonWelles/NewInterpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
