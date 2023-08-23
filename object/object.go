433ab993168b8cfcd790d5963b61f7984acbb8ea
cole <cole@pop-os.localdomain>
2023-08-23 11:03:19 -0400

    initial string parsing and evaluation

--- object/object.go
+++ object/object.go
@@ -15,6 +15,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERRORS"
 	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ 		 = "STRING"
 )
 
 type ObjecType string
@@ -110,3 +111,10 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjecType {return STRING_OBJ}
+func (s *String) Inspect() string {return s.Value}
