5445c4e8ed10f573af592afe59c785e17b5db622
ColesonWelles <cahh38@protonmail.com>
2023-08-28 15:20:04 -0400

    array literal evaluation implemented

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Restarting my efforts to work through Thorsten Ball's "Writing An Interpreter In
 
 2) Take up Ball's challenge for his readers to refactor the interpreter to accept Unicode instead of just ASCII
 characters.
-
-(B: p161)
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -381,3 +381,19 @@ func TestBuiltinFunctions(t *testing.T) {
 		}
 	}
 }
+
+func TestParsingArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	evaluated := testEval(input)
+	result, ok := evaluated.(*object.Array)
+	if !ok {
+		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if len(result.Elements) != 3 {
+		t.Fatalf("array has wrong number of elements, got=%d", len(result.Elements))
+	}
+
+	
+}
