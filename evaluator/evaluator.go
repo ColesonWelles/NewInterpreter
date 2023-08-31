9effe9cda514c71fec93c6e3bcada30ee75bbc71
ColesonWelles <cahh38@protonmail.com>
2023-08-31 11:33:47 -0700

    array index evaluation & add additional built-in array functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -78,6 +78,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 	return nil
 }
@@ -265,12 +275,12 @@ func isError(obj object.Object) bool {
 func evalIdentifier(
 	node *ast.Identifier,
 	env *object.Environment,
-	) object.Object {
+) object.Object {
 	if val, ok := env.Get(node.Value); ok {
-	return val
+		return val
 	}
 	if builtin, ok := builtins[node.Value]; ok {
-	return builtin
+		return builtin
 	}
 	return newError("identifier not found: " + node.Value)
 }
@@ -303,7 +313,7 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 
 	default:
 		return newError("not a function: %s", fn.Type())
-}
+	}
 }
 
 func extendFunctionEnv(
@@ -328,15 +338,35 @@ func unwrapReturnValue(obj object.Object) object.Object {
 }
 
 func evalStringInfixExpression(
-	operator string, 
+	operator string,
 	left, right object.Object,
 ) object.Object {
 	if operator != "+" {
 		return newError("unknown operator: %s %s %s",
-	left.Type(), operator, right.Type())
+			left.Type(), operator, right.Type())
 	}
 
 	leftVal := left.(*object.String).Value
 	rightVal := right.(*object.String).Value
 	return &object.String{Value: leftVal + rightVal}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -329,7 +329,7 @@ func TestStringLiteral(t *testing.T) {
 	input := `"Hello World"`
 
 	evaluated := testEval(input)
-	str, ok :=  evaluated.(*object.String)
+	str, ok := evaluated.(*object.String)
 	if !ok {
 		t.Fatalf("object is not a string. got=%T (%+v)", evaluated, evaluated)
 	}
@@ -353,7 +353,7 @@ func TestStringConcatenation(t *testing.T) {
 
 func TestBuiltinFunctions(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected interface{}
 	}{
 		{`len("")`, 0},
@@ -395,5 +395,60 @@ func TestParsingArrayLiterals(t *testing.T) {
 		t.Fatalf("array has wrong number of elements, got=%d", len(result.Elements))
 	}
 
-	
+}
+
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			"[1, 2, 3][0]",
+			1,
+		},
+		{
+			"[1, 2, 3][1]",
+			2,
+		},
+		{
+			"[1, 2, 3][2]",
+			3,
+		},
+		{"let i = 0; [1][i];",
+			1,
+		},
+		{
+			"[1, 2, 3][1 + 1];",
+			3,
+		},
+		{
+			"let myArray = [1, 2, 3]; myArray[2];",
+			3,
+		},
+		{
+			"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2];",
+			6,
+		},
+		{
+			"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i]",
+			2,
+		},
+		{
+			"[1, 2, 3][3]",
+			nil,
+		},
+		{
+			"[1, 2, 3][-1]",
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
 }
